b37e9b30cad69f82fe8f4d6bcbdeff0fe43f6455
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-07-23 12:44:34 +0100

    refactor

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,13 +7,6 @@ const { getOne } = require("../dataAcess/organization");
 
 const createUser = async (req, res) => {
   try {
-
-    const organizationName = req.headers.tenant; // This should be the db_name of the organization
-    const organization = await orgServices.getOne({
-     db_name: organizationName,
-    });
-
-    // console.log(organization)
     const user = await userServices.createUser(req.body);
     const sessionAuth = await sessionService.createApiKey(
       user.data._id,
@@ -21,15 +14,7 @@ const createUser = async (req, res) => {
       req.get("User-Agent")
     );
 
-    // const organizationName = req.headers.tenant;
-    // console.log(organizationName);
-    // let { id} = req.headers.tenant;
-    // const organization = await orgServices.getOne(
-    //   {name: organizationName}
-    // );
-    // console.log(organization);
     user.data.sessionAuth = sessionAuth.data;
-    user.data.organization = organization._id;
     await user.data.save();
     
     return res.status(user.statusCode).json(user);
